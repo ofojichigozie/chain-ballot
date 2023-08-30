51c84b25f0a625b40f2c0edd36b08adcc6c40cca
ofojichigozie <ofojichigozie@gmail.com>
2023-08-30 17:16:14 +0100

    Connected to new ballot contract

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,7 +13,7 @@ const supportedChain = {
 };
 
 const addresses = {
-  ballot: "0xc39c9744f6599f454337e566601551bBEEe83bDA",
+  ballot: "0xbe7808500054405984D731E28181f648c8cAEc38",
 };
 
 const abis = {
