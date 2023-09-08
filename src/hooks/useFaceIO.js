f9e46e21f03a4b97b1e4f2d2858f972b0df452fb
ofojichigozie <ofojichigozie@gmail.com>
2023-09-08 16:45:05 +0100

    Updated faceio key

--- src/hooks/useFaceIO.js
+++ src/hooks/useFaceIO.js
@@ -5,7 +5,7 @@ const useFaceIO = () => {
 
   useEffect(() => {
     // eslint-disable-next-line
-    const faceio = new faceIO("fioab097");
+    const faceio = new faceIO("fioa6362");
     setFaceio(faceio);
   }, []);
 
